72db6e0ba7b0134653216fc2d0666b06a0799a81
henluo <122192113+hihenluo@users.noreply.github.com>
2025-12-10 17:25:50 +0000

    feat: add UpgradePage route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 // src/App.tsx
 import { useEffect } from "react";
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
+  BrowserRouter as Router,
+  Routes,
+  Route,
 } from "react-router-dom";
 import { sdk } from "@farcaster/miniapp-sdk";
 
@@ -12,30 +12,32 @@ import GamePage from "./pages/GamePage";
 import InfoPage from "./pages/InfoPage";
 import { UserPage } from "./pages/UserPage";
 import MintPage from "./pages/MintPage";
-import SwapPage from "./pages/SwapPage"; // Import SwapPage
+import SwapPage from "./pages/SwapPage"; 
+import UpgradePage from "./pages/UpgradePage"; 
 import "./index.css";
 
 function App() {
-  useEffect(() => {
-    sdk.actions
-      .ready()
-      .then(() => sdk.actions.addMiniApp())
-      .catch((e) => console.warn("SDK ready error:", e));
-  }, []);
+  useEffect(() => {
+    sdk.actions
+      .ready()
+      .then(() => sdk.actions.addMiniApp())
+      .catch((e) => console.warn("SDK ready error:", e));
+  }, []);
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="game" element={<GamePage />} />
-        <Route path="info" element={<InfoPage />} />
-        <Route path="user" element={<UserPage />} />
-        <Route path="mint" element={<MintPage />} />
-        <Route path="swap" element={<SwapPage />} />
-        <Route path="*" element={<HomePage />} />
-      </Routes>
-    </Router>
-  );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="game" element={<GamePage />} />
+        <Route path="info" element={<InfoPage />} />
+        <Route path="user" element={<UserPage />} />
+        <Route path="mint" element={<MintPage />} />
+        <Route path="swap" element={<SwapPage />} />
+        <Route path="upgrade" element={<UpgradePage />} /> 
+        <Route path="*" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
